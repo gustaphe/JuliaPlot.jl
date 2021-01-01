318a1f6d548987d1ad42d23e88ab80e972e8ec8a
Jared Hughes <jahughes241@gmail.com>
2021-01-01 03:09:39 -0800

    Create ~/Images/Julia directory if not already present

--- resources/generatejuliabackground.sh
+++ resources/generatejuliabackground.sh
@@ -1,5 +1,6 @@
 #! /bin/zsh
 notify-send "Generating new Julia background"
+mkdir -p ~/Images/Julia
 /usr/local/bin/generatejuliabackground.jl
 feh --bg-fill --no-xinerama ~/Images/Julia/background.png
 notify-send "Background generated" -i ~/Images/Julia/background.png
